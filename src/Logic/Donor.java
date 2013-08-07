d16ecb67dd61bdb5c23bdc76deefb10e389a8005
Sadie <23sadie@gmail.com>
2013-08-06 18:51:42 -0700

    Created donor class

---
+++ src/Logic/Donor.java
@@ -0,0 +1,30 @@
+package Logic;
+
+public class Donor {
+	private String fname;
+	private String lname;
+	private String email;
+	
+	
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
