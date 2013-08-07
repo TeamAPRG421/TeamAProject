56e1ce952cd31cfbedd4f9cdce43494272588a85
MikeMoore3456 <Michael.Moore2@gmail.com>
2013-08-06 19:01:28 -0700

    Added Util Class

---
+++ src/Logic/Util.java
@@ -0,0 +1,37 @@
+package Logic;
+
+import javax.swing.table.DefaultTableModel;
+
+public class Util {
+	public static DefaultTableModel CreateHeader(String[] columnNames)
+	{
+		DefaultTableModel model = new DefaultTableModel();
+		
+		for(String str: columnNames)
+		{
+			model.addColumn((Object)str);
+		}
+		
+		return model;
+	}
+	
+	
+	public static  boolean isNumber(String str)
+	{
+		String exp = "\\d*";		
+		return str.matches(exp);
+	}
+	
+	public static boolean isDecimal(String str)
+	{
+		String exp = "[0-9.]+";
+		return str.matches(exp);
+	}
+	
+	public static boolean isValidEmail(String str)
+	{
+		String exp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+		return str.matches(exp);
+	}
+	
+}
