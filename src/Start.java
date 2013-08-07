2de4aeeb6c4650f10b845554051f56838e1ffffb
Unarufire <unarufire@gmail.com>
2013-08-06 19:14:22 -0700

    Changed GUI Start Window

--- src/Start.java
+++ src/Start.java
@@ -11,6 +11,10 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+import java.awt.Font;
+import javax.swing.JTable;
+import javax.swing.JScrollBar;
 
 
 public class Start {
@@ -19,6 +23,8 @@ public class Start {
 	private JTextField txtFirstName;
 	private JTextField txtLastName;
 	private JTextField txtEmailAddress;
+	private JTable table;
+	private JTextField textDonationAmount;
 
 	/**
 	 * Launch the application.
@@ -48,66 +54,69 @@ public class Start {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 832, 591);
+		frame.setBounds(100, 100, 727, 408);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblFirstName = new JLabel("First Name");
 		lblFirstName.setHorizontalAlignment(SwingConstants.CENTER);
-		lblFirstName.setBounds(24, 25, 138, 27);
+		lblFirstName.setBounds(30, 82, 138, 27);
 		frame.getContentPane().add(lblFirstName);
 		
 		txtFirstName = new JTextField();
-		txtFirstName.setBounds(24, 49, 138, 20);
+		txtFirstName.setBounds(30, 106, 138, 20);
 		frame.getContentPane().add(txtFirstName);
 		txtFirstName.setColumns(10);
 		
 		JLabel lblLastName = new JLabel("Last Name");
 		lblLastName.setHorizontalAlignment(SwingConstants.CENTER);
-		lblLastName.setBounds(24, 81, 138, 14);
+		lblLastName.setBounds(30, 138, 138, 14);
 		frame.getContentPane().add(lblLastName);
 		
 		txtLastName = new JTextField();
-		txtLastName.setBounds(24, 96, 138, 20);
+		txtLastName.setBounds(30, 153, 138, 20);
 		frame.getContentPane().add(txtLastName);
 		txtLastName.setColumns(10);
 		
 		JLabel lblEmailAddress = new JLabel("Email Address");
 		lblEmailAddress.setHorizontalAlignment(SwingConstants.CENTER);
-		lblEmailAddress.setBounds(24, 137, 138, 14);
+		lblEmailAddress.setBounds(30, 186, 138, 20);
 		frame.getContentPane().add(lblEmailAddress);
 		
 		txtEmailAddress = new JTextField();
-		txtEmailAddress.setBounds(24, 151, 138, 20);
+		txtEmailAddress.setBounds(30, 208, 138, 20);
 		frame.getContentPane().add(txtEmailAddress);
 		txtEmailAddress.setColumns(10);
 		
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setForeground(Color.BLUE);
-		menuBar.setBounds(24, 208, 97, 21);
-		frame.getContentPane().add(menuBar);
+		JComboBox cmbCharity = new JComboBox();
+		cmbCharity.setBounds(30, 52, 138, 22);
+		frame.getContentPane().add(cmbCharity);
 		
-		JMenu mnCharityOptions = new JMenu("Charity Options");
-		menuBar.add(mnCharityOptions);
+		JLabel lblCharityName = new JLabel("Charity Name:");
+		lblCharityName.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblCharityName.setBounds(30, 13, 116, 26);
+		frame.getContentPane().add(lblCharityName);
 		
-		JMenuBar menuBar_1 = new JMenuBar();
-		menuBar_1.setForeground(Color.BLUE);
-		menuBar_1.setBounds(244, 208, 97, 21);
-		frame.getContentPane().add(menuBar_1);
-		
-		JMenu mnAmountDonated = new JMenu("Amount Donated ");
-		menuBar_1.add(mnAmountDonated);
-		
-		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
-		mnAmountDonated.add(mntmNewMenuItem);
+		table = new JTable();
+		table.setBounds(190, 47, 507, 270);
+		frame.getContentPane().add(table);
 		
 		JButton btnSubmit = new JButton("Submit");
-		btnSubmit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnSubmit.setBounds(34, 449, 89, 23);
+		btnSubmit.setBounds(49, 288, 97, 25);
 		frame.getContentPane().add(btnSubmit);
+		
+		textDonationAmount = new JTextField();
+		textDonationAmount.setBounds(30, 253, 138, 22);
+		frame.getContentPane().add(textDonationAmount);
+		textDonationAmount.setColumns(10);
+		
+		JLabel lblDonationAmount = new JLabel("Donation Amount");
+		lblDonationAmount.setBounds(49, 235, 116, 16);
+		frame.getContentPane().add(lblDonationAmount);
+		
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(676, 141, 21, 48);
+		frame.getContentPane().add(scrollBar);
 		
 		JMenuBar menuBar_2 = new JMenuBar();
 		frame.setJMenuBar(menuBar_2);
